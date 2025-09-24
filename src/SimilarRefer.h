209fd724ae6b352a49d2a06d7246e187c69fca6d
Stepanov Andrei <andrei.s@coppermountaintech.com>
2025-09-24 15:53:57 +0300

    refactor: add .clang-format file

--- src/SimilarRefer.h
+++ src/SimilarRefer.h
@@ -4,23 +4,30 @@
 
 #include <type_traits>
 
-namespace ScL { namespace Detail
+namespace ScL::Detail
 {
-    template < typename _Type, typename _Refer >
+    template <typename _Type, typename _Refer>
     struct SimilarReferHelper
     {
-        static_assert( ::std::is_reference< _Refer >::value, "The template parameter _Refer must to be a reference type." );
+        static_assert(::std::is_reference<_Refer>::value,
+            "The template parameter _Refer must to be a reference type.");
 
-        using ValueFromRefer = ::std::remove_reference_t< _Refer >;
-        static constexpr bool is_lvalue = ::std::is_lvalue_reference< _Refer >::value;
-        static constexpr bool is_rvalue = ::std::is_rvalue_reference< _Refer >::value;
-        static constexpr bool is_const = ::std::is_const< ValueFromRefer >::value;
-        static constexpr bool is_volatile = ::std::is_volatile< ValueFromRefer >::value;
+        using ValueFromRefer = ::std::remove_reference_t<_Refer>;
+        static constexpr bool is_lvalue = ::std::is_lvalue_reference<_Refer>::value;
+        static constexpr bool is_rvalue = ::std::is_rvalue_reference<_Refer>::value;
+        static constexpr bool is_const = ::std::is_const<ValueFromRefer>::value;
+        static constexpr bool is_volatile = ::std::is_volatile<ValueFromRefer>::value;
 
-        using VCheckedType = ::std::conditional_t< is_volatile, ::std::add_volatile_t< _Type >, _Type >;
-        using CVCheckedType = ::std::conditional_t< is_const, ::std::add_const_t< VCheckedType >, VCheckedType >;
-        using LCVCheckedType = ::std::conditional_t< is_lvalue, ::std::add_lvalue_reference_t< CVCheckedType >, CVCheckedType >;
-        using Type = ::std::conditional_t< is_rvalue, ::std::add_rvalue_reference_t< LCVCheckedType >, LCVCheckedType >;
+        using VCheckedType = ::std::conditional_t<is_volatile, ::std::add_volatile_t<_Type>, _Type>;
+        using CVCheckedType = ::std::conditional_t<is_const,
+            ::std::add_const_t<VCheckedType>,
+            VCheckedType>;
+        using LCVCheckedType = ::std::conditional_t<is_lvalue,
+            ::std::add_lvalue_reference_t<CVCheckedType>,
+            CVCheckedType>;
+        using Type = ::std::conditional_t<is_rvalue,
+            ::std::add_rvalue_reference_t<LCVCheckedType>,
+            LCVCheckedType>;
 
         // NOTE: _Type может являться reference типом, в этом случае применение свойств
         // lvalue_reference/rvalue_reference может иметь результат отличный от _Refer.
@@ -28,16 +35,17 @@ namespace ScL { namespace Detail
         // NOTE: _Type может изначально иметь const/volatile свойства, в этом
         // случае результат может не соответствовать этим свойствам для _Refer.
 
-        //static_assert( ::std::is_reference< _Type >::value
-        //    || ::ScL::Feature::is_similar< Type, _Refer >::value, "Result Type must to be similar _Refer" );
+        // static_assert( ::std::is_reference< _Type >::value
+        //     || ::ScL::Feature::is_similar< Type, _Refer >::value, "Result Type must to be similar
+        //     _Refer" );
     };
-}}
+} // namespace ScL::Detail
 
 namespace ScL
 {
-    template < typename _Type, typename _Refer >
-    using SimilarRefer = typename ::ScL::Detail::SimilarReferHelper< _Type, _Refer >::Type;
-        //!< Наделение типа свойствами ссылки.
-}
+    //! Наделение типа свойствами ссылки
+    template <typename _Type, typename _Refer>
+    using SimilarRefer = typename ::ScL::Detail::SimilarReferHelper<_Type, _Refer>::Type;
+} // namespace ScL
 
 #endif
--- src/ValueOf.h
+++ src/ValueOf.h
@@ -2,17 +2,18 @@
 #ifndef SCL_META_VALUE_OF_H
 #define SCL_META_VALUE_OF_H
 
+namespace ScL::Detail
+{
+    //! This struct must be specialized for not standard cases.
+    template <typename _Type>
+    struct ValueOf : _Type
+    {};
+} // namespace ScL::Detail
+
 namespace ScL
 {
-    namespace Detail
-    {
-        template < typename _Type >
-        struct ValueOf : _Type {};
-            //!< This struct must be specialized for not standard cases.
-    }
-
-    template < typename _Type >
-    auto constexpr value_of = Detail::ValueOf< _Type >::value;
-}
+    template <typename _Type>
+    constexpr auto value_of = Detail::ValueOf<_Type>::value;
+} // namespace ScL
 
 #endif
