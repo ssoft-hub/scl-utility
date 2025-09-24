da97cb242da4605f189f703f630f4caf481c913f
ssoft <and.stepanov@gmail.com>
2025-09-24 15:53:57 +0300

    refactor: add .clang-format file

--- src/Qualifier.h
+++ src/Qualifier.h
@@ -2,46 +2,46 @@
 #ifndef SCL_UTILITY_AS_CONST_H
 #define SCL_UTILITY_AS_CONST_H
 
-#include <memory>
 #include <type_traits>
+#include <utility>
 
 namespace ScL
 {
-    template < typename _Type >
-    constexpr ::std::add_const_t< _Type > & asConst ( _Type & value ) noexcept
+    template <typename _Type>
+    constexpr ::std::add_const_t<_Type> & asConst(_Type & value) noexcept
     {
         return value;
     }
 
-    template < typename _Type >
-    constexpr ::std::add_const_t< _Type > && asConst ( _Type && value ) noexcept
+    template <typename _Type>
+    constexpr ::std::add_const_t<_Type> && asConst(_Type && value) noexcept
     {
-        return ::std::forward< ::std::add_const_t< _Type > && >( value );
+        return ::std::forward< ::std::add_const_t<_Type> &&>(value);
     }
 
-    template < typename _Type >
-    constexpr ::std::add_volatile_t< _Type > & asVolatile ( _Type & value ) noexcept
+    template <typename _Type>
+    constexpr ::std::add_volatile_t<_Type> & asVolatile(_Type & value) noexcept
     {
         return value;
     }
 
-    template < typename _Type >
-    constexpr ::std::add_volatile_t< _Type > && asVolatile ( _Type && value ) noexcept
+    template <typename _Type>
+    constexpr ::std::add_volatile_t<_Type> && asVolatile(_Type && value) noexcept
     {
-        return ::std::forward< ::std::add_volatile_t< _Type > && >( value );
+        return ::std::forward< ::std::add_volatile_t<_Type> &&>(value);
     }
 
-    template < typename _Type >
-    constexpr ::std::add_cv_t< _Type > & asConstVolatile ( _Type & value ) noexcept
+    template <typename _Type>
+    constexpr ::std::add_cv_t<_Type> & asConstVolatile(_Type & value) noexcept
     {
         return value;
     }
 
-    template < typename _Type >
-    constexpr ::std::add_cv_t< _Type > && asConstVolatile ( _Type && value ) noexcept
+    template <typename _Type>
+    constexpr ::std::add_cv_t<_Type> && asConstVolatile(_Type && value) noexcept
     {
-        return ::std::forward< ::std::add_cv_t< _Type > && >( value );
+        return ::std::forward< ::std::add_cv_t<_Type> &&>(value);
     }
-}
+} // namespace ScL
 
 #endif
